d2118eb685e208f76a3f00658888475a3c09de3a
Sachin Mehta <137069741+Ekaksh77@users.noreply.github.com>
2024-08-06 21:13:09 +0200

    Add files via upload

---
+++ Movie Analysis/Movie.sql
@@ -0,0 +1,40 @@
+WITH T1 AS (
+    SELECT [Seat Number], Gender
+    FROM Movie$
+),
+T2 AS (
+    SELECT [Seat Number], Gender
+    FROM Movie$
+),
+T3 AS (
+    SELECT [Seat Number], Gender
+    FROM Movie$
+)
+
+SELECT T1.[Seat Number], T1.Gender, T2.[Seat Number], T2.Gender , T3.[Seat Number], T3.Gender
+FROM T1 JOIN T2 
+ON T1.[Seat Number] = T2.[Seat Number] - 1
+AND T1.Gender = T2.Gender
+JOIN T3 ON T2.[Seat Number] = T3.[Seat Number]-1
+AND T2.Gender = T3.Gender
+
+where T1.Gender = 'M'
+
+
+/*****************************************************************/
+create Table Name_1(Cx_Name varchar(50))
+insert into Name_1 values ('karan'),('sagar'),('udit'),('max')
+
+select *
+from Name_1
+
+/*Q. Change the first letter of Name into Capital letter*/
+select *, upper(left(Cx_Name,1))+ SUBSTRING(Cx_Name,2,len(Cx_Name)) as Customer
+from Name_1
+
+select *, UPPER(LEFT(Cx_Name,1)) + SUBSTRING(Cx_Name,2, len(Cx_Name)) as CXName
+from Name_1
+
+
+select *, UPPER(LEFT(Cx_Name,1))+ SUBSTRING(Cx_Name,2,len(Cx_Name)) as Nameeee
+from Name_1
---
+++ Movie Analysis/README.md
@@ -0,0 +1,5 @@
+
+SQL Query
+
+1. Write a query to find out 3 Male consucutive seat Number and Gender
+2. Change the first letter of Name into Capital letter
